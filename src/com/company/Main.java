c2f7a68a3e2204d0b1b6c69e1c8a66be639ec712
Hdey68 <ouniche.sofiane@gmail.com>
2021-01-27 14:03:43 -0500

    Sun added but with some errors

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,5 +8,11 @@ public class Main {
         FactorX.Sun=1;
         FactorX.Moon=3;
         FactorX.Planets=2;
+
+
+        SUN Freon= new SUN ();
+        Freon.color="red";
+        Freon.size="7000km";
+        Freon.Temp="40000c";
     }
 }
